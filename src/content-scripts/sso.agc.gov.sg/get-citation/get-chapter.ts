0cf8559feef527a665dbb3223850a2d5777937bb
eugbyte <eugenetham1994@gmail.com>
2021-09-10 11:01:53 +0800

    chore: update

--- src/content-scripts/sso.agc.gov.sg/get-citation/get-chapter.ts
+++ src/content-scripts/sso.agc.gov.sg/get-citation/get-chapter.ts
@@ -1,14 +1,18 @@
 import { StringImpl } from 'src/services/string-service';
+import startCase from 'lodash.startcase';
 
 interface Services {
   stringService: StringImpl;
 }
 
 export function getChapter ({ stringService }: Services): string {
-  const nav = document.getElementById('tocNav') as HTMLElement;
-  const span = nav.querySelector('span') as HTMLSpanElement;
-  let chapter: string = span.innerText;
+  let chapter = (document.getElementById('aT-') as HTMLElement).innerText;
   // add a space before the "("
   chapter = chapter.replace(/\(/, ' (');
-  return stringService.removeLineBreaks(chapter);
+  chapter = startCase(chapter.toLowerCase());
+  console.log({chapter});
+
+  const actNumber = (document.querySelector(".actNo") as HTMLElement).innerText;
+  const fullChapter = `${chapter} ${actNumber}`;
+  return stringService.removeLineBreaks(fullChapter);
 }
